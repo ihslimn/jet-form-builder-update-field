cac8cd97b4ab5616609dc406e467e4eed1ff22f1
Mykhailo Kulinich <ihslimn@gmail.com>
2023-12-13 22:05:10 +0200

    FIX: do not add column when not editing form

--- compatibility/jet-booking/manager.php
+++ compatibility/jet-booking/manager.php
@@ -11,22 +11,38 @@ if ( ! defined( 'WPINC' ) ) {
 
 class Jet_Booking {
 
-    public function __construct() {
-        add_action( 'jet-engine/register-macros', array( $this, 'register_macros' ) );
-        add_filter( 'jet-abaf/db/additional-db-columns', array( $this, 'add_apartment_unit_column' ), 999 );
-    }
+	public function __construct() {
+		add_action( 'jet-engine/register-macros', array( $this, 'register_macros' ) );
+		add_filter( 'jet-abaf/db/additional-db-columns', array( $this, 'add_apartment_unit_column' ), 999 );
+	}
 
-    public function register_macros() {
-        require_once Plugin::instance()->plugin_path( 'compatibility/jet-booking/macros/available-units.php' );
+	public function register_macros() {
+		require_once Plugin::instance()->plugin_path( 'compatibility/jet-booking/macros/available-units.php' );
 		new \JFB_Update_Field\Macros\Available_Units();
-    }
+	}
 
-    public function add_apartment_unit_column( $columns ) {
+	public function add_apartment_unit_column( $columns ) {
 
-        $columns[] = 'apartment_unit';
+		if ( ! is_admin() ) {
+			return $columns;
+		}
 
-        return $columns;
+		if ( ! empty( $_GET['post'] ) ) {
+			
+			$post_type = get_post_type( $_GET['post'] );
 
-    }
+			if ( $post_type !== 'jet-form-builder' ) {
+				return $columns;
+			}
+
+		} elseif ( empty( $_GET['post_type'] ) || $_GET['post_type'] !== 'jet-form-builder' ) {
+			return $columns;
+		}
+
+		$columns[] = 'apartment_unit';
+
+		return $columns;
+
+	}
 
 }
